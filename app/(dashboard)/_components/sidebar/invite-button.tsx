6345c5376e465d7d1d7864182829ccf30c6809a8
Vladimir <slight1989@gmail.com>
2024-02-26 17:48:40 +0400

    add: EmptyOrg component

--- app/(dashboard)/_components/sidebar/invite-button.tsx
+++ app/(dashboard)/_components/sidebar/invite-button.tsx
@@ -1,8 +1,7 @@
 import { Plus } from 'lucide-react';
 import { OrganizationProfile } from '@clerk/nextjs';
-import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
-
 import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 
 const InviteButton = () => {
   return (
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,15 @@
+'use client';
+
+import { useOrganization } from '@clerk/nextjs';
+import EmptyOrg from './_components/sidebar/empty-org';
+
 const Dashboardpage = () => {
-  return <div>Dashboard root page</div>;
+  const { organization } = useOrganization();
+  return (
+    <div className=" flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? <EmptyOrg /> : <>Board List</>}
+    </div>
+  );
 };
 
 export default Dashboardpage;
